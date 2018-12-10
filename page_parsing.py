b4eb7364935a17809c3c884f6b213252f364eca9
Timofei <timofei.ganjusev@tudeng.ut.ee>
2018-12-10 03:53:47 +0200

    have solve proble with page counting

--- page_parsing.py
+++ page_parsing.py
@@ -1,3 +1,10 @@
+## find the element on the HTML page that contains queue results
+## find how many results there are total for the given city (i.e parish 1061 aka TALLINN)
+## in order to have a starting page and calculate ending page
+## in order to successfully scrape one page & loop to the next one
+## in order to scrape all the results on all pages
+## b-c I would like to save them all to a database and analyze
+
 from bs4 import BeautifulSoup
 from urllib import request
 
@@ -5,11 +12,8 @@ url = 'https://www.kv.ee/?act=search.simple&last_deal_type=1&page=2&orderby=ob&p
 content = request.urlopen(url)
 raw_html = content.read()
 soup = BeautifulSoup(raw_html, 'html.parser')
-page_number_html = soup.find_all('div', class_="pagination")
-
-def page_number_finder(tag):
-	numbers = tag.find(class_="active")
-	print(numbers)
-	return numbers
-
-page_number_finder(page_number_html)
+tag = soup.select('.jump-pagination-list > li:nth-of-type(3)')[0]
+print(tag.text)
+## 'Only the following pseudo-classes are implemented: nth-of-type.
+##.jump-pagination-list > li:nth-last-child(2)
+## working fine
