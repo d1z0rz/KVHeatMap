90d597f9275d4a1ca85c5bc3a85a552732b62c89
Timofei <timofei.ganjusev@tudeng.ut.ee>
2018-12-10 04:02:33 +0200

    something changed

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from urllib import request
 from bs4 import BeautifulSoup
 import re
 import geocoder
+import time
+u = time.time()
 
 PRICE = 'object-price-value'
 PRICE_M2 = 'object-m2-price'
@@ -10,7 +12,7 @@ ADDRESS = 'object-title-a'
 def getPrice(tag, type):
 	res = tag.find(class_=type)
 	raw_text = res.text
-	return format_price(raw_text) 
+	return format_price(raw_text)
 
 def format_price(raw_price):
 	stripped = raw_price.strip().replace(u'\xa0', '')
@@ -28,7 +30,7 @@ def getLocation(address):
 	raw_address = geocoder.yandex(address)
 	location = raw_address.latlng
 	return location
-url = 'https://www.kv.ee/?act=search.simple&last_deal_type=1&page=2&orderby=ob&page_size=100&deal_type=1&dt_select=1&county=1&search_type=new&parish=1061'
+url = 'https://www.kv.ee/?act=search.simple&last_deal_type=1&page=2&orderby=ob&page_size=500&deal_type=1&dt_select=1&county=1&search_type=new&parish=1061'
 content = request.urlopen(url)
 raw_html = content.read()
 soup = BeautifulSoup(raw_html, 'html.parser')
@@ -41,5 +43,8 @@ for object in objects:
 	address = getAddress(object)
 	location = getLocation(address)
 	result.append({'price':price, 'price_m2':price_m2, 'address':address,'location':location})
-print(result)
+#print(result)
+u2 = time.time()
+kek = u2 - u
+print(kek)
 ## returns an array of objects [ {coords, address, price, pricem2} ]
